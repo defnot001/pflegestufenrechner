f6c9f6f6963727a885d04256096207539b21d99a
defnot001 <marc.miller994@gmail.com>
2024-09-08 18:45:35 +0200

    add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import { HomePage } from './routes/index.tsx';
 import './index.css';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { ErrorPage } from './routes/error.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
