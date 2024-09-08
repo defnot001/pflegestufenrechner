bcc645066dbf24ca9911b749475681382e0d5926
defnot001 <marc.miller994@gmail.com>
2024-09-08 18:44:56 +0200

    remove base path since that is no longer necessary

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pflegestufenrechner/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
 });
