cae5b6e14360402abf14dc45285ebc594c09da05
defnot001 <defnot001@gmail.com>
2024-10-09 22:13:39 +0200

    add custom font and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,25 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        'dw-light': '#B2E3F7',
+        'dw-medium': '#0378AB',
+        'dw-dark': '#162E65',
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'ui-sans-serif', 'system-ui'],
+      },
+      fontWeight: {
+        thin: '100',
+        extralight: '200',
+        light: '300',
+        normal: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+        extrabold: '800',
+        black: '900',
+      },
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
