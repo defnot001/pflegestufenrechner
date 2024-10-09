0be38741db96ba58e9f8dd73e9201b01a15bb646
defnot001 <defnot001@gmail.com>
2024-10-09 23:55:58 +0200

    add DW Logo

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,17 +1,19 @@
 import { Link } from 'react-router-dom';
 import { FileText, Scale, User, HelpingHand } from 'lucide-react';
+import DWLogo from '../../assets/dw_logo.svg';
 
 export function MainHeader() {
   return (
     <header className='my-4'>
-      <h1 className='text-4xl font-bold text-center mb-4 bg-gradient-to-tr from-dw-400 to-dw-200 text-transparent bg-clip-text'>
-        Pflegestufenrechner
-      </h1>
+      <div className='flex justify-center my-2'>
+        <img src={DWLogo} alt='Diakoniewerk Logo' className='w-52' />
+      </div>
+      <h1 className='text-3xl font-bold text-center mb-4 text-dw-300'>Pflegestufenrechner</h1>
       <p className='text-center text-gray-600 mb-4'>
         Tool zur Berechnung der Pflegestufe auf Basis des österreichischen Pflegegeldgesetzes.
       </p>
       <nav className='flex justify-center gap-x-3 flex-wrap'>
-        <Link to='/about' className='flex items-center text-dw-300 hover:text-dw-400'>
+        <Link to='/help' className='flex items-center text-dw-300 hover:text-dw-400'>
           <HelpingHand className='w-4 h-4 mr-1' />
           <span>Hilfe</span>
         </Link>
